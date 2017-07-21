68aeb70e54dc1652938ad4e569ae3d667e719c41
Vincent Castigliola <vincentcastigliola@Vincents-MacBook-Air.local>
2017-07-21 10:19:37 -0500

    chat

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -6,17 +6,28 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 $scope.getMessages = function(){
   messageService.getMessages().then(function(response){
     $scope.messages = response.data;
+    var d = response.data;
+    var date = (d.createdAt)
+    for (item in d){
+    var date = new Date(d[item].createdAt)
+    }
+
+
+      console.log(response.data[0].createdAt)
+
   })
 }
+
+
 
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
   $scope.postMessage = function(message){
-    messageService.postMessage(message).then(function(message){
-    })
+    messageService.postMessage(message)
   }
+
 
 
   //uncomment this code when your getMessages function is finished
--- js/messageService.js
+++ js/messageService.js
@@ -12,7 +12,6 @@ angular.module('chatroom').service('messageService', function($http){
     })
   }
 
-
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmountain.com/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
